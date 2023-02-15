956915860682111d4fffc6844fe864b1b91b0d12
Michal Vavřík <mvavrik@redhat.com>
2023-02-15 01:20:21 +0100

    alt test

--- extensions/oidc/deployment/src/test/java/io/quarkus/oidc/test/ExportRealmTestCase.java
+++ extensions/oidc/deployment/src/test/java/io/quarkus/oidc/test/ExportRealmTestCase.java
@@ -3,11 +3,12 @@ package io.quarkus.oidc.test;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.function.Consumer;
 
 import org.jboss.shrinkwrap.api.asset.StringAsset;
-import org.junit.jupiter.api.AfterEach;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
@@ -16,42 +17,41 @@ import io.quarkus.test.QuarkusDevModeTest;
 
 public class ExportRealmTestCase {
 
-    private static Class<?>[] testClasses = {
-            UnprotectedResource.class
-    };
     private static final String APPLICATION_PROPERTIES = "application.properties";
     private static final String REALM_FILE_NAME = "realm.json";
-    private Path realmDir;
+    private static Path realmDir;
 
     @RegisterExtension
     static final QuarkusDevModeTest test = new QuarkusDevModeTest()
-            .withApplicationRoot((jar) -> jar
-                    .addClasses(testClasses)
-                    .addAsResource(new StringAsset("quarkus.keycloak.devservices.enabled=true"), APPLICATION_PROPERTIES));
+            .withApplicationRoot(new Consumer<JavaArchive>() {
+                @Override
+                public void accept(JavaArchive jar) {
+                    try {
+                        realmDir = Files.createTempDirectory("quarkus-dev-mode-export-realm-test-dir");
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                    jar
+                            .addAsResource(new StringAsset(String.format("quarkus.keycloak.devservices.export-realm-path=%s",
+                                    realmDir.toAbsolutePath())), APPLICATION_PROPERTIES);
+                }
+            });
 
-    @BeforeEach
-    public void createRealmDir() throws IOException {
-        realmDir = Files.createTempDirectory("quarkus-dev-mode-export-realm-test-dir");
-    }
-
-    @AfterEach
-    public void deleteRealmDir() throws IOException {
-        FileUtil.deleteDirectory(realmDir);
+    @AfterAll
+    public static void assertResult() throws IOException {
+        try {
+            final var realmFilePath = realmDir.resolve(REALM_FILE_NAME);
+            Assertions.assertTrue(Files.exists(realmFilePath));
+            final var realmAsStr = Files.readString(realmFilePath);
+            Assertions.assertTrue(realmAsStr.contains("master-realm"));
+        } finally {
+            FileUtil.deleteDirectory(realmDir);
+        }
     }
 
     @Test
     public void test() {
-        // we start without realm export path set, therefore we know it's optional
-        final var realmFilePath = realmDir.resolve(REALM_FILE_NAME);
-        Assertions.assertFalse(Files.exists(realmFilePath));
-        // set export dir
-        test.modifyResourceFile(APPLICATION_PROPERTIES,
-                fileAsStr -> String.format("quarkus.keycloak.devservices.export-realm-path=%s", realmDir.toAbsolutePath()));
-        // stop Keycloak container
-        test.modifyResourceFile(APPLICATION_PROPERTIES,
-                fileAsStr -> fileAsStr.replace("=true", "=false"));
-        // validate exported file
-        Assertions.assertTrue(Files.exists(realmFilePath));
+        // result is asserted after application is stopped as that's when container is stopped and realm exported
     }
 
 }
