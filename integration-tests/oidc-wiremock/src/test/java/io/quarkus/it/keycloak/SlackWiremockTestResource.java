4a4a12833f94d5c49735436b82486e6dd6c2d640
Michal Vavřík <mvavrik@redhat.com>
2024-10-23 21:01:41 +0200

    wip

--- integration-tests/oidc-wiremock/src/test/java/io/quarkus/it/keycloak/SlackWiremockTestResource.java
+++ integration-tests/oidc-wiremock/src/test/java/io/quarkus/it/keycloak/SlackWiremockTestResource.java
@@ -4,7 +4,7 @@ import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static com.github.tomakehurst.wiremock.client.WireMock.containing;
 import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
 import static com.github.tomakehurst.wiremock.client.WireMock.get;
-import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlMatching;
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
 
 import java.io.Closeable;
@@ -17,18 +17,20 @@ import com.github.tomakehurst.wiremock.matching.AnythingPattern;
 public class SlackWiremockTestResource implements Closeable {
 
     private static final Logger LOG = Logger.getLogger(SlackWiremockTestResource.class);
+    private static final int PORT = 8188;
     private final WireMockServer server;
 
     SlackWiremockTestResource() {
-        var config = wireMockConfig().port(8188).globalTemplating(true);
+        var config = wireMockConfig().port(PORT).globalTemplating(true);
         this.server = new WireMockServer(config);
+        LOG.info("Starting Slack mock on port " + PORT);
         this.server.start();
         configureStubs();
     }
 
     private void configureStubs() {
         server.stubFor(
-                get(urlEqualTo("/.well-known/openid-configuration"))
+                get(urlMatching("/.well-known/openid-configuration.*"))
                         .willReturn(aResponse()
                                 .withHeader("Content-Type", "application/json")
                                 .withBody(
@@ -54,46 +56,50 @@ public class SlackWiremockTestResource implements Closeable {
                                                 """)));
 
         server.stubFor(
-                get(urlEqualTo("/openid/connect/authorize"))
+                get(urlMatching("/openid/connect/authorize.*"))
                         .withQueryParam("response_type", equalTo("code"))
                         .withQueryParam("client_id", equalTo("7917304849541.7920910519717"))
                         .withQueryParam("scope", containing("openid"))
                         .withQueryParam("scope", containing("email"))
                         .withQueryParam("scope", containing("profile"))
                         .withQueryParam("scope", containing("profile"))
-                        .withQueryParam("redirect_uri", equalTo("https%3A%2F%2Flocalhost%3A8443%2F"))
+                        .withQueryParam("redirect_uri", equalTo("http://localhost:8081/code-flow-slack"))
                         .withQueryParam("state", new AnythingPattern())
                         .withQueryParam("team", equalTo("quarkus-oidc-slack-demo"))
                         .willReturn(aResponse()
                                 .withStatus(302)
                                 .withHeader("Set-Cookie", "{{request.headers.Set-Cookie}}")
                                 .withHeader("Content-Type", "text/html")
-                                .withHeader("Location", "https://localhost:8443?code=7917304849541.79239831"
+                                .withHeader("Location", "http://localhost:8081?code=7917304849541.79239831"
                                         + "24323.1f4c41812b286422cbce183a9f083fa58f7c2761c281c2be483a376694f56274&state"
                                         + "={{request.query.state}}")
                                 .withBody("")));
 
         server.stubFor(
-                get(urlEqualTo("/openid/connect/keys"))
+                get(urlMatching("/openid/connect/keys.*"))
                         .willReturn(aResponse()
+                                .withHeader("Set-Cookie", "{{request.headers.Set-Cookie}}")
                                 .withHeader("Content-Type", "application/json")
-                                .withBody("{\n" +
-                                        "  \"keys\" : [\n" +
-                                        "    {\n" +
-                                        "      \"kid\": \"1\",\n" +
-                                        "      \"kty\":\"RSA\",\n" +
-                                        "      \"n\":\"iJw33l1eVAsGoRlSyo-FCimeOc-AaZbzQ2iESA3Nkuo3TFb1zIkmt0kzlnWVGt48dkaIl13Vdefh9hqw_r9yNF8xZqX1fp0PnCWc5M_TX_ht5fm9y0TpbiVmsjeRMWZn4jr3DsFouxQ9aBXUJiu26V0vd2vrECeeAreFT4mtoHY13D2WVeJvboc5mEJcp50JNhxRCJ5UkY8jR_wfUk2Tzz4-fAj5xQaBccXnqJMu_1C6MjoCEiB7G1d13bVPReIeAGRKVJIF6ogoCN8JbrOhc_48lT4uyjbgnd24beatuKWodmWYhactFobRGYo5551cgMe8BoxpVQ4to30cGA0qjQ\",\n"
-                                        +
-                                        "      \"e\":\"AQAB\"\n" +
-                                        "    }\n" +
-                                        "  ]\n" +
-                                        "}")));
+                                .withBody(
+                                        """
+                                                {
+                                                  "keys": [
+                                                    {
+                                                      "e": "AQAB",
+                                                      "n": "zQqzXfb677bpMKw0idKC5WkVLyqk04PWMsWYJDKqMUUuu_PmzdsvXBfHU7tcZiNoHDuVvGDqjqnkLPEzjXnaZY0DDDHvJKS0JI8fkxIfV1kNy3DkpQMMhgAwnftUiSXgb5clypOmotAEm59gHPYjK9JHBWoHS14NYEYZv9NVy0EkjauyYDSTz589aiKU5lA-cePG93JnqLw8A82kfTlrJ1IIJo2isyBGANr0YzR-d3b_5EvP7ivU7Ph2v5JcEUHeiLSRzIzP3PuyVFrPH659Deh-UAsDFOyJbIcimg9ITnk5_45sb_Xcd_UN6h5I7TGOAFaJN4oi4aaGD4elNi_K1Q",
+                                                      "kty": "RSA",
+                                                      "kid": "mB2MAyKSn555isd0EbdhKx6nkyAi9xLq8rvCEb_nOyY",
+                                                      "alg": "RS256"
+                                                    }
+                                                  ]
+                                                }
+                                                """)));
     }
 
     @Override
     public void close() {
         server.stop();
-        LOG.info("Keycloak was shut down");
+        LOG.info("Slack mock was shut down");
     }
 
 }
