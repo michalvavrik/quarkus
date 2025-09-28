22599de745b3ef7a108a68f679b4feb8ff64a1ae
Michal Vavřík <michal.vavrik@aol.com>
2025-09-28 11:40:38 +0200

    fix(vertx-http,security): schedule next handler once auth finishes

--- extensions/vertx-http/runtime/src/main/java/io/quarkus/vertx/http/runtime/security/AbstractHttpAuthorizer.java
+++ extensions/vertx-http/runtime/src/main/java/io/quarkus/vertx/http/runtime/security/AbstractHttpAuthorizer.java
@@ -24,9 +24,12 @@ import io.quarkus.security.spi.runtime.AuthorizationFailureEvent;
 import io.quarkus.security.spi.runtime.AuthorizationSuccessEvent;
 import io.quarkus.security.spi.runtime.BlockingSecurityExecutor;
 import io.quarkus.security.spi.runtime.SecurityEventHelper;
+import io.smallrye.common.vertx.VertxContext;
 import io.smallrye.mutiny.Uni;
 import io.smallrye.mutiny.subscription.UniSubscriber;
 import io.smallrye.mutiny.subscription.UniSubscription;
+import io.vertx.core.Context;
+import io.vertx.core.Handler;
 import io.vertx.ext.web.RoutingContext;
 
 /**
@@ -73,6 +76,11 @@ abstract class AbstractHttpAuthorizer {
             SecurityIdentity augmentedIdentity,
             List<HttpSecurityPolicy> permissionCheckers) {
         if (index == permissionCheckers.size()) {
+            if (index == 0) {
+                // no HTTP permission checks
+                routingContext.next();
+                return;
+            }
             QuarkusHttpUser currentUser = (QuarkusHttpUser) routingContext.user();
             if (augmentedIdentity != null) {
                 if (!augmentedIdentity.isAnonymous()
@@ -89,7 +97,14 @@ abstract class AbstractHttpAuthorizer {
                         new AuthorizationSuccessEvent(currentUser == null ? null : currentUser.getSecurityIdentity(),
                                 Map.of(RoutingContext.class.getName(), routingContext)));
             }
-            routingContext.next();
+            Context context = VertxContext.getOrCreateDuplicatedContext();
+            // let subscriber callbacks to finish before continuing
+            context.runOnContext(new Handler<Void>() {
+                @Override
+                public void handle(Void unused) {
+                    routingContext.next();
+                }
+            });
             return;
         }
         //get the current checker
