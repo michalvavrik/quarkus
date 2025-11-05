9f231cca8b639f2bafbe4f19965cea08c590a511
Michal Vavřík <michal.vavrik@aol.com>
2025-11-05 20:17:48 +0100

    wip

---
+++ integration-tests/hibernate-orm-data/src/main/java/io/quarkus/it/hibernate/processor/data/pudefault/SecuredMyRepository.java
@@ -0,0 +1,40 @@
+package io.quarkus.it.hibernate.processor.data.pudefault;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import jakarta.annotation.security.RolesAllowed;
+import jakarta.data.Order;
+import jakarta.data.repository.CrudRepository;
+import jakarta.data.repository.Delete;
+import jakarta.data.repository.Find;
+import jakarta.data.repository.Insert;
+import jakarta.data.repository.Query;
+import jakarta.data.repository.Repository;
+
+@Repository
+public interface SecuredMyRepository extends CrudRepository<MyEntity, Integer> {
+
+    @RolesAllowed("root")
+    @Insert
+    void insertRootRole(MyEntity entity);
+
+    @RolesAllowed("admin")
+    @Insert
+    void insertAdminRole(MyEntity entity);
+
+    @RolesAllowed("${donald}")
+    @Find
+    Stream<MyEntity> findAllForDonald(Order<MyEntity> order);
+
+    @RolesAllowed("${george}")
+    @Find
+    Stream<MyEntity> findAllForGeorge(Order<MyEntity> order);
+
+    @Query("select e from MyEntity e where e.name like :name")
+    List<MyEntity> findByName(String name);
+
+    @Delete
+    void delete(String name);
+
+}
