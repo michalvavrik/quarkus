7dd93784a813334e05414f28ff59db04682f78a4
Michal Vavřík <michal.vavrik@aol.com>
2025-11-06 15:41:44 +0100

    wip

--- extensions/security/deployment/src/main/java/io/quarkus/security/deployment/DenyUnannotatedPredicate.java
+++ extensions/security/deployment/src/main/java/io/quarkus/security/deployment/DenyUnannotatedPredicate.java
@@ -6,14 +6,20 @@ import java.util.function.Predicate;
 import org.jboss.jandex.ClassInfo;
 import org.jboss.jandex.MethodInfo;
 
-import io.quarkus.security.spi.SecurityTransformerUtils;
+import io.quarkus.security.spi.SecurityTransformerHelper;
 
 final class DenyUnannotatedPredicate implements Predicate<ClassInfo> {
+
+    private final SecurityTransformerHelper helper;
+
+    DenyUnannotatedPredicate(SecurityTransformerHelper helper) {
+        this.helper = helper;
+    }
 
     @Override
     public boolean test(ClassInfo classInfo) {
         List<MethodInfo> methods = classInfo.methods();
-        return !SecurityTransformerUtils.hasSecurityAnnotation(classInfo)
-                && methods.stream().anyMatch(SecurityTransformerUtils::hasSecurityAnnotation);
+        return !helper.hasSecurityAnnotation(classInfo)
+                && methods.stream().anyMatch(helper::hasSecurityAnnotation);
     }
 }
