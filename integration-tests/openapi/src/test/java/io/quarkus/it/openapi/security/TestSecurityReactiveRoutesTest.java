7b7d803c5b6e7c94d402e53b39b1314b126ef0fa
Michal Vavřík <michal.vavrik@aol.com>
2025-11-28 17:09:19 +0100

    wip

--- integration-tests/openapi/src/test/java/io/quarkus/it/openapi/security/TestSecurityReactiveRoutesTest.java
+++ integration-tests/openapi/src/test/java/io/quarkus/it/openapi/security/TestSecurityReactiveRoutesTest.java
@@ -1,17 +1,25 @@
 package io.quarkus.it.openapi.security;
 
+import static io.quarkus.it.openapi.security.TestSecurityResource.TEST_HEADER_NAME;
+import static org.apache.http.params.CoreConnectionPNames.SO_TIMEOUT;
 import static org.hamcrest.Matchers.is;
 
+import java.net.SocketTimeoutException;
+
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import io.quarkus.test.common.http.TestHTTPEndpoint;
 import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.security.TestSecurity;
 import io.restassured.RestAssured;
+import io.restassured.config.HttpClientConfig;
 
 @QuarkusTest
 @TestHTTPEndpoint(TestSecurityResource.class)
 public class TestSecurityReactiveRoutesTest {
+
+    private static final String TEST_HEADER_VALUE = "hush-hush";
 
     @TestSecurity(user = "Martin", roles = "admin")
     @Test
@@ -21,6 +29,38 @@ public class TestSecurityReactiveRoutesTest {
                 .statusCode(200)
                 .header("reactive-routes-filter", is("true"))
                 .body(is("Martin"));
+    }
+
+    @TestSecurity(user = "Martin", roles = "admin")
+    @Test
+    public void testCdiRequestActiveAfterTimeout() {
+        int valueLesserThanDelay = TestSecurityResource.REQUEST_TIMEOUT - 3;
+        // using deprecated constant due to https://github.com/rest-assured/rest-assured/issues/497
+        var config = RestAssured.config()
+                .httpClient(HttpClientConfig.httpClientConfig().setParam(SO_TIMEOUT, valueLesserThanDelay));
+        try {
+            RestAssured.given()
+                    .config(config)
+                    .header(TEST_HEADER_NAME, TEST_HEADER_VALUE)
+                    .get("reactive-routes-with-delayed-response")
+                    .then()
+                    .statusCode(200)
+                    .header("reactive-routes-filter", is("true"))
+                    .body(is("Martin"));
+            Assertions.fail("HTTP request didn't result in a socket timeout exception");
+        } catch (Exception socketTimeoutException) {
+            // yes, this checked exception is thrown even though no method signature declares it
+            if (!(socketTimeoutException instanceof SocketTimeoutException)) {
+                // socket timeout exception is required to verify what happens with the CDI request context after the timeout
+                Assertions.fail("Expected a SocketTimeoutException but got " + socketTimeoutException);
+            }
+        }
+        RestAssured.given()
+                .get("test-header")
+                .then()
+                .statusCode(200)
+                .header("reactive-routes-filter", is("true"))
+                .body(is(TEST_HEADER_NAME + " header value was: " + TEST_HEADER_VALUE));
     }
 
 }
