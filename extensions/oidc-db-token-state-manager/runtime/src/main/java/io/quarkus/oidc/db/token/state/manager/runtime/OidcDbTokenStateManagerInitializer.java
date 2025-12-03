5271e12516b6eb3388ca58d90695203e47dc9788
Michal Vavřík <michal.vavrik@aol.com>
2025-12-03 20:09:06 +0100

    wip

--- extensions/oidc-db-token-state-manager/runtime/src/main/java/io/quarkus/oidc/db/token/state/manager/runtime/OidcDbTokenStateManagerInitializer.java
+++ extensions/oidc-db-token-state-manager/runtime/src/main/java/io/quarkus/oidc/db/token/state/manager/runtime/OidcDbTokenStateManagerInitializer.java
@@ -2,12 +2,14 @@ package io.quarkus.oidc.db.token.state.manager.runtime;
 
 import static io.quarkus.oidc.db.token.state.manager.runtime.OidcDbTokenStateManager.now;
 
+import java.util.Objects;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.BiFunction;
 import java.util.function.Consumer;
 import java.util.function.Function;
 
 import jakarta.enterprise.event.Observes;
+import jakarta.inject.Singleton;
 
 import org.jboss.logging.Logger;
 
@@ -20,7 +22,8 @@ import io.vertx.sqlclient.Pool;
 import io.vertx.sqlclient.Row;
 import io.vertx.sqlclient.RowSet;
 
-public class OidcDbTokenStateManagerInitializer {
+@Singleton
+public final class OidcDbTokenStateManagerInitializer {
 
     private static final Logger LOG = Logger.getLogger(OidcDbTokenStateManagerInitializer.class);
     private static final String FAILED_TO_CREATE_DB_TABLE = "unknown reason, please report the issue and create table manually";
@@ -28,12 +31,18 @@ public class OidcDbTokenStateManagerInitializer {
      * Extra 30 seconds before we delete expired tokens.
      */
     private static final long EXPIRED_EXTRA_GRACE = 30;
-    private static volatile Long timerId = null;
+    private volatile Long timerId = null;
+    private volatile SupportedReactiveSqlClient supportedReactiveSqlClient = null;
 
-    void initialize(@Observes StartupEvent event, OidcDbTokenStateManagerRunTimeConfig config, Vertx vertx, Pool pool,
-            OidcDbTokenStateManagerInitializerProperties initializerProps) {
+    void setSupportedReactiveSqlClient(SupportedReactiveSqlClient supportedReactiveSqlClient) {
+        this.supportedReactiveSqlClient = supportedReactiveSqlClient;
+    }
+
+    void initialize(@Observes StartupEvent event, OidcDbTokenStateManagerRunTimeConfig config, Vertx vertx, Pool pool) {
+        Objects.requireNonNull(supportedReactiveSqlClient);
         if (config.createDatabaseTableIfNotExists()) {
-            createDatabaseTable(pool, initializerProps.createTableDdl, initializerProps.supportsIfTableNotExists);
+            createDatabaseTable(pool, supportedReactiveSqlClient.getCreateTableDdl(config),
+                    supportedReactiveSqlClient.supportsIfTableNotExists);
         }
         periodicallyDeleteExpiredTokens(vertx, pool, config.deleteExpiredDelay().toMillis());
     }
@@ -44,7 +53,7 @@ public class OidcDbTokenStateManagerInitializer {
         }
     }
 
-    private static void periodicallyDeleteExpiredTokens(Vertx vertx, Pool pool, long delayBetweenChecks) {
+    private void periodicallyDeleteExpiredTokens(Vertx vertx, Pool pool, long delayBetweenChecks) {
         timerId = vertx
                 .setPeriodic(5000, delayBetweenChecks, new Handler<Long>() {
 
@@ -140,14 +149,60 @@ public class OidcDbTokenStateManagerInitializer {
         }
     }
 
-    public static final class OidcDbTokenStateManagerInitializerProperties {
+    public enum SupportedReactiveSqlClient {
+        POSTGRESQL(true, "CREATE TABLE IF NOT EXISTS oidc_db_token_state_manager (" +
+                "id VARCHAR(100) PRIMARY KEY, " +
+                "id_token VARCHAR, " +
+                "access_token VARCHAR, " +
+                "refresh_token VARCHAR, " +
+                "access_token_expires_in BIGINT, " +
+                "access_token_scope VARCHAR, " +
+                "expires_in BIGINT NOT NULL)"),
+        DB2(false, "CREATE TABLE oidc_db_token_state_manager ("
+                + "id VARCHAR(100) NOT NULL PRIMARY KEY, "
+                + "id_token VARCHAR(5000), "
+                + "access_token VARCHAR(5000), "
+                + "refresh_token VARCHAR(5000), "
+                + "access_token_expires_in BIGINT, "
+                + "access_token_scope VARCHAR(100), "
+                + "expires_in BIGINT NOT NULL)"),
+        ORACLE(true, "CREATE TABLE IF NOT EXISTS oidc_db_token_state_manager ("
+                + "id VARCHAR2(100), "
+                + "id_token VARCHAR2(5000), "
+                + "access_token VARCHAR2(5000), "
+                + "refresh_token VARCHAR2(5000), "
+                + "access_token_expires_in NUMBER, "
+                + "access_token_scope VARCHAR2(100), "
+                + "expires_in NUMBER NOT NULL, "
+                + "PRIMARY KEY (id))"),
+        MYSQL(true, "CREATE TABLE IF NOT EXISTS oidc_db_token_state_manager ("
+                + "id VARCHAR(100), "
+                + "id_token VARCHAR(5000) NULL, "
+                + "access_token VARCHAR(5000) NULL, "
+                + "refresh_token VARCHAR(5000) NULL, "
+                + "access_token_expires_in BIGINT NULL, "
+                + "access_token_scope VARCHAR(100) NULL, "
+                + "expires_in BIGINT NOT NULL, "
+                + "PRIMARY KEY (id))"),
+        MSSQL(false, "CREATE TABLE oidc_db_token_state_manager ("
+                + "id NVARCHAR(100) PRIMARY KEY, "
+                + "id_token NVARCHAR(MAX), "
+                + "access_token NVARCHAR(MAX), "
+                + "refresh_token NVARCHAR(MAX), "
+                + "access_token_expires_in BIGINT, "
+                + "access_token_scope NVARCHAR(100), "
+                + "expires_in BIGINT NOT NULL)");
 
-        private final String createTableDdl;
         private final boolean supportsIfTableNotExists;
+        private final String createTableDdl;
 
-        OidcDbTokenStateManagerInitializerProperties(String createTableDdl, boolean supportsIfTableNotExists) {
-            this.createTableDdl = createTableDdl;
+        SupportedReactiveSqlClient(boolean supportsIfTableNotExists, String createTableDdl) {
             this.supportsIfTableNotExists = supportsIfTableNotExists;
+            this.createTableDdl = createTableDdl;
+        }
+
+        private String getCreateTableDdl(OidcDbTokenStateManagerRunTimeConfig config) {
+            return createTableDdl;
         }
     }
 }
