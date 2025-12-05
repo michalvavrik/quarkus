3d6cd280a193ef2dc0244c2733da4485f51a65a3
Michal Vavřík <michal.vavrik@aol.com>
2025-12-05 22:12:20 +0100

    wip

--- extensions/resteasy-reactive/rest-csrf/runtime/src/main/java/io/quarkus/csrf/reactive/runtime/RestCsrfConfigHolder.java
+++ extensions/resteasy-reactive/rest-csrf/runtime/src/main/java/io/quarkus/csrf/reactive/runtime/RestCsrfConfigHolder.java
@@ -4,11 +4,17 @@ import static io.quarkus.vertx.http.runtime.security.HttpSecurityConfiguration.g
 
 import jakarta.enterprise.context.ApplicationScoped;
 
+import org.jboss.logging.Logger;
+
+import io.quarkus.runtime.LaunchMode;
 import io.quarkus.vertx.http.runtime.VertxHttpBuildTimeConfig;
 import io.quarkus.vertx.http.runtime.VertxHttpConfig;
 
 @ApplicationScoped
 public class RestCsrfConfigHolder {
+
+    // used in DEV mode to detect changes in the generated token size
+    private static volatile Integer previousTokenSize = null;
 
     private final RestCsrfConfig config;
 
@@ -17,6 +23,14 @@ public class RestCsrfConfigHolder {
             this.config = programmaticConfig;
         } else {
             this.config = config;
+        }
+        if (LaunchMode.current() == LaunchMode.DEVELOPMENT) {
+            if (previousTokenSize != null && previousTokenSize != this.config.tokenSize()) {
+                Logger.getLogger(RestCsrfConfigHolder.class).infof("Generated token size has changed from %d to %d." +
+                        " Thus, previously generated CSRF token is not valid anymore. Consider deleting the '%s' cookie in your browser.",
+                        previousTokenSize, this.config.tokenSize(), this.config.cookieName());
+            }
+            previousTokenSize = this.config.tokenSize();
         }
     }
 
