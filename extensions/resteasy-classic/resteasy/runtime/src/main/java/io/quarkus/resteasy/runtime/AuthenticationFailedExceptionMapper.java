773a156730e32908813547feaaf2c8572c95ece7
Michal Vavřík <michal.vavrik@aol.com>
2026-01-19 21:47:14 +0100

    wip
    Signed-off-by: Michal Vavřík <michal.vavrik@aol.com>

--- extensions/resteasy-classic/resteasy/runtime/src/main/java/io/quarkus/resteasy/runtime/AuthenticationFailedExceptionMapper.java
+++ extensions/resteasy-classic/resteasy/runtime/src/main/java/io/quarkus/resteasy/runtime/AuthenticationFailedExceptionMapper.java
@@ -42,8 +42,10 @@ public class AuthenticationFailedExceptionMapper implements ExceptionMapper<Auth
                         .await().indefinitely();
                 int statusCode = challengeData == null ? 401 : challengeData.status;
                 Response.ResponseBuilder responseBuilder = Response.status(statusCode);
-                if (challengeData != null && challengeData.headerName != null) {
-                    responseBuilder.header(challengeData.headerName.toString(), challengeData.headerContent);
+                if (challengeData != null && challengeData.addHeaders()) {
+                    challengeData.getHeaders()
+                            .forEach((headerName, headerContent) -> responseBuilder.header(headerName.toString(),
+                                    headerContent));
                 }
                 if (LaunchMode.current().isDev() && exception.getMessage() != null && statusCode == 401) {
                     responseBuilder.entity(exception.getMessage());
