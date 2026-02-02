34d1760bedaacebf09c536bd3033da73b3ed86fe
Michal Vavřík <michal.vavrik@aol.com>
2026-02-02 22:29:08 +0100

    wip
    Signed-off-by: Michal Vavřík <michal.vavrik@aol.com>

---
+++ integration-tests/oidc-dpop/src/test/java/io/quarkus/it/keycloak/OidcRichAuthorizationRequestsTest.java
@@ -0,0 +1,42 @@
+package io.quarkus.it.keycloak;
+
+import static io.quarkus.it.keycloak.OidcDPopTest.createWebClient;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.IOException;
+
+import org.htmlunit.TextPage;
+import org.htmlunit.WebClient;
+import org.htmlunit.html.HtmlForm;
+import org.htmlunit.html.HtmlPage;
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.oidc.runtime.OidcUtils;
+import io.quarkus.test.junit.QuarkusTest;
+
+@QuarkusTest
+class OidcRichAuthorizationRequestsTest {
+
+    @Test
+    void testAuthorizationDetails() throws IOException {
+        try (final WebClient webClient = createWebClient()) {
+            HtmlPage page = webClient.getPage("http://localhost:8081/single-page-app/login-rar");
+
+            assertEquals("Sign in to quarkus", page.getTitleText());
+
+            HtmlForm loginForm = page.getForms().get(0);
+
+            loginForm.getInputByName("username").setValueAttribute("alice");
+            loginForm.getInputByName("password").setValueAttribute("alice");
+
+            TextPage textPage = loginForm.getButtonByName("login").click();
+
+            String jwt = textPage.getContent();
+            var jwtJsonObject = OidcUtils.decodeJwtContent(jwt);
+            assertEquals("alice", jwtJsonObject.getString("preferred_username"));
+
+            webClient.getCookieManager().clearCookies();
+        }
+    }
+
+}
