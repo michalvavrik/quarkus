87ed3771fad78644367d53bca91b2bbd7392a18f
Michal Vavřík <michal.vavrik@aol.com>
2026-02-07 19:32:22 +0100

    refactor(oidc-common): extract ClientAssertionProvider from K8 provider
    As a pre-step for https://github.com/quarkusio/quarkus/issues/52232, we agreed to extract `ClientAssertionProvider` so that later, we can introduce SPIFFE one.
    
    Signed-off-by: Michal Vavřík <michal.vavrik@aol.com>

--- extensions/oidc/runtime/src/main/java/io/quarkus/oidc/runtime/OidcTenantConfig.java
+++ extensions/oidc/runtime/src/main/java/io/quarkus/oidc/runtime/OidcTenantConfig.java
@@ -1378,7 +1378,6 @@ public interface OidcTenantConfig extends OidcClientCommonConfig {
 
         /**
          * Configures the `authorization_details` request parameter string fields of the authorization request.
-         * Please note that the `type` field is required. If the `type` is not configured, validation will fail.
          * For example, if you configure the `quarkus.oidc.authentication.rar.simple.type=openid_credential`
          * property, following `authorization_details` request parameter will be added to the authorization request:
          *
