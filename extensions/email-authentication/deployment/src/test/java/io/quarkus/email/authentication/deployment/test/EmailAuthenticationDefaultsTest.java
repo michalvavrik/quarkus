743a72513ceb491ff7931da506eeb245922970ac
Michal Vavřík <michal.vavrik@aol.com>
2026-02-21 21:08:42 +0100

    wip
    Signed-off-by: Michal Vavřík <michal.vavrik@aol.com>

---
+++ extensions/email-authentication/deployment/src/test/java/io/quarkus/email/authentication/deployment/test/EmailAuthenticationDefaultsTest.java
@@ -0,0 +1,65 @@
+package io.quarkus.email.authentication.deployment.test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.is;
+
+import jakarta.inject.Inject;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import io.quarkus.mailer.MockMailbox;
+import io.quarkus.test.QuarkusUnitTest;
+
+class EmailAuthenticationDefaultsTest {
+
+    /**
+     * These chars correspond to the chars in the email authentication mechanism.
+     */
+    private static final String ALLOWED_CHARS = "23456789BCDFGHJKMNPQRSTVWXYZ";
+
+    private static final TestEmailAuthenticationHelper HELPER = new TestEmailAuthenticationHelper(
+            "/generate-email-authentication-token", "/j_security_check", "Your verification code", "Your verification code is",
+            "j_username", "j_token");
+
+    @RegisterExtension
+    static final QuarkusUnitTest APP = new QuarkusUnitTest().withApplicationRoot(HELPER.getAppConfig());
+
+    @Inject
+    MockMailbox mailbox;
+
+    @BeforeEach
+    void reset() {
+        HELPER.clear();
+    }
+
+    @Test
+    void testCompleteFlowSuccess() {
+        String username = "Vaclav";
+        String emailAddress = "test-vaclav@quarkus.io";
+        TestTrustedIdentityProvider.reset().addUser(username, emailAddress, "admin");
+
+        String targetPath = "/secured/admin";
+        HELPER.goTo(targetPath).statusCode(302).header("location", containsString("/login.html"));
+        HELPER.assertCookie("quarkus-redirect-location", targetPath);
+
+        HELPER.requestTokenFor(username).statusCode(302).header("location", containsString("/token.html"));
+        var cookie = HELPER.assertCookiePresent("quarkus-credential-request");
+        assertThat(cookie).extracting(c -> c.isSecure()).isEqualTo(true);
+        String token = HELPER.assertEmailAndGetToken(mailbox, emailAddress);
+        assertThat(token).hasSize(15);
+        for (char c : token.toCharArray()) {
+            assertThat(Character.toString(c)).isSubstringOf(ALLOWED_CHARS);
+        }
+
+        HELPER.submitToken(token).statusCode(302).header("location", containsString(targetPath));
+        HELPER.assertCookieMissing("quarkus-credential-request");
+        HELPER.assertCookieMissing("quarkus-redirect-location");
+        HELPER.assertCookiePresent("quarkus-credential");
+
+        HELPER.goTo(targetPath).statusCode(200).body(is(username + ":" + targetPath));
+    }
+
+}
---
+++ extensions/email-authentication/deployment/src/test/java/io/quarkus/email/authentication/deployment/test/TestEmailAuthenticationHelper.java
@@ -0,0 +1,134 @@
+package io.quarkus.email.authentication.deployment.test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.function.Consumer;
+
+import org.apache.http.cookie.Cookie;
+import org.awaitility.Awaitility;
+import org.hamcrest.Matchers;
+import org.jboss.shrinkwrap.api.asset.StringAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+
+import io.quarkus.mailer.Mail;
+import io.quarkus.mailer.MockMailbox;
+import io.restassured.RestAssured;
+import io.restassured.filter.cookie.CookieFilter;
+import io.restassured.response.ValidatableResponse;
+
+final class TestEmailAuthenticationHelper {
+
+    static final String FROM = "security@quarkus.io";
+
+    private final String tokenGenerationPath;
+    private final String postLocation;
+    private final String emailSubject;
+    private final String emailTextStart;
+    private CookieFilter cookieFilter;
+    private final String usernameParameter;
+    private final String tokenParameter;
+
+    TestEmailAuthenticationHelper(String tokenGenerationPath, String postLocation, String emailSubject, String emailTextStart,
+            String usernameParameter, String tokenParameter) {
+        this.tokenGenerationPath = tokenGenerationPath;
+        this.postLocation = postLocation;
+        this.emailSubject = emailSubject;
+        this.emailTextStart = emailTextStart;
+        this.usernameParameter = usernameParameter;
+        this.tokenParameter = tokenParameter;
+        this.cookieFilter = new CookieFilter();
+    }
+
+    ValidatableResponse requestTokenFor(String username) {
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+        return RestAssured.given()
+                .filter(cookieFilter)
+                .formParam(usernameParameter, username)
+                .post(tokenGenerationPath)
+                .then();
+    }
+
+    void clear() {
+        cookieFilter = new CookieFilter();
+    }
+
+    String assertEmailAndGetToken(MockMailbox mailbox, String emailAddress) {
+        var emails = awaitEmailWithToken(mailbox, emailAddress);
+        var firstMailAsserter = assertThat(emails).hasSize(1).first();
+        firstMailAsserter.extracting(Mail::getSubject).isEqualTo(emailSubject);
+        assertThat(firstMailAsserter.extracting(Mail::getText).actual()).startsWith(emailTextStart);
+        String emailAuthToken = emails.get(0).getText()
+                .trim()
+                .transform(t -> {
+                    var arr = t.split(" ");
+                    return arr[arr.length - 1];
+                });
+        assertThat(emailAuthToken).isNotEmpty();
+        return emailAuthToken;
+    }
+
+    Consumer<JavaArchive> getAppConfig() {
+        return (jar) -> jar
+                .addAsResource(new StringAsset("""
+                        quarkus.mailer.from=%s
+                        quarkus.http.auth.permission.secured.paths=/secured*
+                        quarkus.http.auth.permission.secured.policy=authenticated
+                        quarkus.http.auth.permission.admin.paths=/secured/admin*
+                        quarkus.http.auth.permission.admin.policy=admin
+                        quarkus.http.auth.policy.admin.roles-allowed=admin
+                        """.formatted(FROM)), "application.properties")
+                .addClasses(TestTrustedIdentityProvider.class, TestEmailAuthenticationHelper.class, TestPathHandler.class);
+    }
+
+    ValidatableResponse goTo(String targetPath) {
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+        return RestAssured
+                .given()
+                .redirects().follow(false)
+                .filter(cookieFilter)
+                .get(targetPath)
+                .then();
+    }
+
+    Cookie assertCookiePresent(String cookieName) {
+        return assertCookie(cookieName, null);
+    }
+
+    Cookie assertCookie(String cookieName, String value) {
+        var cookieAsserter = assertThat(cookieFilter.getCookieStore().getCookies());
+        cookieAsserter.isNotEmpty();
+        cookieAsserter.extracting(Cookie::getName).anyMatch(cookieName::equalsIgnoreCase);
+        var cookie = cookieAsserter.actual().stream().filter(c -> cookieName.equalsIgnoreCase(c.getName()))
+                .findFirst().get();
+        var cookieValueAsserter = assertThat(cookie).extracting(Cookie::getValue).asString();
+        if (value == null) {
+            cookieValueAsserter.isNotEmpty();
+        } else {
+            cookieValueAsserter.contains(value);
+        }
+        return cookie;
+    }
+
+    ValidatableResponse submitToken(String token) {
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+        return RestAssured.given()
+                .filter(cookieFilter)
+                .redirects().follow(false)
+                .formParam(tokenParameter, token)
+                .post(postLocation)
+                .then();
+    }
+
+    void assertCookieMissing(String cookieName) {
+        assertThat(cookieFilter.getCookieStore().getCookies()).extracting(Cookie::getName)
+                .noneMatch(cookieName::equalsIgnoreCase);
+    }
+
+    private static List<Mail> awaitEmailWithToken(MockMailbox mailbox, String address) {
+        return Awaitility.await()
+                .atMost(Duration.ofSeconds(10))
+                .until(() -> mailbox.getMailsSentTo(address), Matchers.not(Matchers.emptyIterable()));
+    }
+}
