bb06c0eeacc50de8a2b536da3b241d2efae6778d
Michal Vavřík Dev Automation <dev-automation@michalvavrik.net>
2026-08-22 20:51:06 +0000

    WIP sync

--- extensions/security/runtime-spi/src/main/java/io/quarkus/security/spi/runtime/PermissionToActionUtil.java
+++ extensions/security/runtime-spi/src/main/java/io/quarkus/security/spi/runtime/PermissionToActionUtil.java
@@ -1,48 +1,18 @@
 package io.quarkus.security.spi.runtime;
 
-import java.util.Objects;
-
 public final class PermissionToActionUtil {
 
-    public static final class ParsedPermission {
-        private final String name;
-        private final String action;
+    public sealed interface ParsedPermission {
+        String name();
 
-        private ParsedPermission(String name, String action) {
-            this.name = name;
-            this.action = action;
-        }
+        String action();
 
-        public String name() {
-            return name;
+        default boolean hasAction() {
+            return action() != null;
         }
+    }
 
-        public String action() {
-            return action;
-        }
-
-        public boolean hasAction() {
-            return action != null;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o)
-                return true;
-            if (!(o instanceof ParsedPermission that))
-                return false;
-            return Objects.equals(name, that.name) && Objects.equals(action, that.action);
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(name, action);
-        }
-
-        @Override
-        public String toString() {
-            return "ParsedPermission[name=" + name + ", action=" + action + "]";
-        }
+    record ParsedPermissionImpl(String name, String action) implements ParsedPermission {
     }
 
     private PermissionToActionUtil() {
