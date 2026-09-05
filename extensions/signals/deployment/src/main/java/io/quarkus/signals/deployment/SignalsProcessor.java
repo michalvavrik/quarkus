7d3fcbe9571b5345e318fbd01626c8cfd42fa455
Michal Vavřík <dev@michalvavrik.net>
2026-09-05 23:33:01 +0200

    wip

--- extensions/signals/deployment/src/main/java/io/quarkus/signals/deployment/SignalsProcessor.java
+++ extensions/signals/deployment/src/main/java/io/quarkus/signals/deployment/SignalsProcessor.java
@@ -79,6 +79,7 @@ import io.quarkus.signals.runtime.impl.InvokerReceiver;
 import io.quarkus.signals.runtime.impl.InvokerReceiver.InvokerReceiverInfo;
 import io.quarkus.signals.runtime.impl.ReceiverManager;
 import io.quarkus.signals.runtime.impl.RequestContextInterceptor;
+import io.quarkus.signals.runtime.impl.SecurityIntegration;
 import io.quarkus.signals.runtime.impl.SignalBeanCreator;
 import io.quarkus.signals.runtime.impl.SignalsRecorder;
 import io.quarkus.signals.runtime.impl.SignalsRecorder.SignalsContext;
@@ -446,7 +447,7 @@ class SignalsProcessor {
     }
 
     @BuildStep
-    void registerBeans(BuildProducer<AdditionalBeanBuildItem> beans,
+    void registerBeans(BuildProducer<AdditionalBeanBuildItem> beans, Capabilities capabilities,
             ReceiverExecutorImplementationBuildItem receiverExecutorImplementation) {
         AdditionalBeanBuildItem.Builder builder = AdditionalBeanBuildItem.builder();
         builder.addBeanClasses(ReceiverManager.class, RequestContextInterceptor.class);
@@ -456,6 +457,11 @@ class SignalsProcessor {
             default -> throw new IllegalArgumentException(
                     "Unexpected value: " + receiverExecutorImplementation.getImplementation());
         }
+
+        if (capabilities.isPresent(Capability.SECURITY)) {
+            builder.addBeanClass(SecurityIntegration.class);
+        }
+
         beans.produce(builder.build());
     }
 
--- extensions/signals/deployment/src/test/java/io/quarkus/signals/deployment/test/SecurityIntegrationTest.java
+++ extensions/signals/deployment/src/test/java/io/quarkus/signals/deployment/test/SecurityIntegrationTest.java
@@ -2,11 +2,12 @@ package io.quarkus.signals.deployment.test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.stream.Stream;
 
-import jakarta.annotation.security.RolesAllowed;
 import jakarta.enterprise.context.control.ActivateRequestContext;
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
@@ -14,89 +15,82 @@ import jakarta.inject.Singleton;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
+import io.quarkus.security.Authenticated;
 import io.quarkus.security.UnauthorizedException;
 import io.quarkus.security.identity.CurrentIdentityAssociation;
 import io.quarkus.security.identity.SecurityIdentity;
-import io.quarkus.security.test.utils.IdentityMock;
+import io.quarkus.security.runtime.QuarkusPrincipal;
+import io.quarkus.security.runtime.QuarkusSecurityIdentity;
 import io.quarkus.signals.Receives;
 import io.quarkus.signals.Signal;
-import io.quarkus.signals.spi.ReceiverInterceptor;
-import io.quarkus.signals.spi.SignalMetadataEnricher;
 import io.quarkus.test.QuarkusExtensionTest;
-import io.smallrye.common.annotation.Identifier;
-import io.smallrye.mutiny.Uni;
 
 public class SecurityIntegrationTest extends AbstractSignalTest {
 
     @RegisterExtension
     static final QuarkusExtensionTest test = new QuarkusExtensionTest()
             .withApplicationRoot(root -> root.addClasses(
-                    Cmd.class, MyReceivers.class, SecurityIntegration.class, IdentityMock.class));
+                    AuthenticatedCmd.class, MyReceivers.class));
 
     @Inject
-    Signal<Cmd> signal;
+    Signal<AuthenticatedCmd> signal;
+
+    @Inject
+    CurrentIdentityAssociation identityAssociation;
 
     @ActivateRequestContext
     @Test
-    public void testIntegration() {
-        MyReceivers.CMDS.clear();
+    public void testAuthenticationRequired() {
+        MyReceivers.AUTHENTICATED_CMDS.clear();
 
-        IdentityMock.setUpAuth(IdentityMock.ADMIN);
-        String result = signal.reactive().request(new Cmd("Hello"), String.class)
-                .ifNoItem().after(defaultTimeout()).fail()
-                .await().indefinitely();
-        assertEquals("hello", result);
+        Stream.of("admin", "user").map(SecurityIntegrationTest::createSecurityIdentity).forEach(authenticatedUser -> {
+            identityAssociation.setIdentity(authenticatedUser);
+            String result = signal.reactive().request(new AuthenticatedCmd("Hello"), String.class)
+                    .ifNoItem().after(defaultTimeout()).fail()
+                    .await().indefinitely();
+            assertEquals("hello " + authenticatedUser.getPrincipal().getName(), result);
+        });
 
         assertThrows(UnauthorizedException.class, () -> {
-            IdentityMock.setUpAuth(IdentityMock.ANONYMOUS);
-            signal.reactive().request(new Cmd("Hi"), String.class)
+            identityAssociation.setIdentity(createSecurityIdentity(""));
+            signal.reactive().request(new AuthenticatedCmd("Hi"), String.class)
                     .ifNoItem().after(defaultTimeout()).fail()
                     .await().indefinitely();
         });
-        assertEquals(1, MyReceivers.CMDS.size());
-        assertEquals("Hello", MyReceivers.CMDS.get(0).value());
+
+        assertEquals(2, MyReceivers.AUTHENTICATED_CMDS.size());
+        assertTrue(MyReceivers.AUTHENTICATED_CMDS.stream().map(AuthenticatedCmd::value).allMatch(v -> v.startsWith("Hello")));
+    }
+
+    private static SecurityIdentity createSecurityIdentity(String name) {
+        return QuarkusSecurityIdentity.builder()
+                .setAnonymous(name.isEmpty())
+                .setPrincipal(new QuarkusPrincipal(name))
+                .addRole(name)
+                .build();
     }
 
     // --- Signal types ---
 
-    record Cmd(String value) {
+    record AuthenticatedCmd(String value) {
     }
 
     // --- Receivers ---
 
-    @RolesAllowed("admin")
+    @Authenticated
     @Singleton
     public static class MyReceivers {
 
-        static final List<Cmd> CMDS = new CopyOnWriteArrayList<>();
-
-        String process(@Receives Cmd cmd) {
-            CMDS.add(cmd);
-            return cmd.value().toLowerCase();
-        }
-
-    }
-
-    // --- Enrichers, interceptors ---
-
-    @Identifier("quarkus.security")
-    @Singleton
-    public static class SecurityIntegration implements SignalMetadataEnricher, ReceiverInterceptor {
+        static final List<AuthenticatedCmd> AUTHENTICATED_CMDS = new CopyOnWriteArrayList<>();
 
         @Inject
-        CurrentIdentityAssociation currentIdentity;
+        SecurityIdentity securityIdentity;
 
-        @Override
-        public void enrich(EnrichmentContext context) {
-            context.putMetadata("identity", currentIdentity.getDeferredIdentity());
+        String process(@Receives AuthenticatedCmd cmd) {
+            AUTHENTICATED_CMDS.add(cmd);
+            return cmd.value().toLowerCase() + " " + securityIdentity.getPrincipal().getName();
         }
 
-        @SuppressWarnings("unchecked")
-        @Override
-        public Uni<Object> intercept(InterceptionContext context) {
-            currentIdentity.setIdentity((Uni<SecurityIdentity>) context.signalContext().metadata().get("identity"));
-            return context.proceed();
-        }
     }
 
 }
